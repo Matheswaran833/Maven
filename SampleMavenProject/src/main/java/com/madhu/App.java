f5a4a3d017b87af4674cd765676638174eb49f1a
Matheswaranp <mathesswaran@gmail.com>
2025-07-17 22:17:52 +0200

    maven

---
+++ SampleMavenProject/src/main/java/com/madhu/App.java
@@ -0,0 +1,18 @@
+package com.madhu;
+
+import java.util.ResourceBundle;
+
+public class App {
+     
+	public int userLogin(String in_user, String in_pwd)
+	{
+		ResourceBundle rb=ResourceBundle.getBundle("config");
+		String UserName=rb.getString("username");
+		String Password=rb.getString("password");
+		
+		if(in_user.equals(UserName) && in_pwd.equals(Password))
+			return 1;
+		else
+			return 0;
+	}
+}
